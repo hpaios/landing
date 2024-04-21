c212975aa5b5e433cb0475dda8487eba1a7d0825
Anna <>
2024-04-21 13:06:05 +0300

    styled nav, logo

--- src/index.js
+++ src/index.js
@@ -4,31 +4,31 @@ import './fonts/fonts.scss';
 window.addEventListener('load', (e) => {
   e.preventDefault();
   const lang = localStorage.getItem('lang');
-  lang === 'uk' ? changeLanguageUK() : changeLanguageRU();
+  lang === 'ua' ? changeLanguageUA() : changeLanguageRU();
 })
 
-const changeLanguageUK = () => { 
+const changeLanguageUA = () => { 
   const ruLang = document.getElementsByClassName("ru-lang");
-  const ukLang = document.getElementsByClassName("uk-lang");
-  localStorage.setItem('lang', 'uk');
-  for (let i = 0; i < ukLang.length; i++) {
-    ukLang[i].style.display = 'inline-block';
+  const uaLang = document.getElementsByClassName("ua-lang");
+  localStorage.setItem('lang', 'ua');
+  for (let i = 0; i < uaLang.length; i++) {
+    uaLang[i].style.display = 'inline-block';
     ruLang[i].style.display = 'none';
   }
 };
 
 const changeLanguageRU = () => { 
   const ruLang = document.getElementsByClassName("ru-lang");
-  const ukLang = document.getElementsByClassName("uk-lang");
+  const uaLang = document.getElementsByClassName("ua-lang");
   localStorage.setItem('lang', 'ru');
-  for (let i = 0; i < ukLang.length; i++) {
+  for (let i = 0; i < uaLang.length; i++) {
     ruLang[i].style.display = 'inline-block';
-    ukLang[i].style.display = 'none';
+    uaLang[i].style.display = 'none';
   }
 };
 
-const ukLang = document.getElementById('uk-lang');
-ukLang.addEventListener('click', changeLanguageUK)
+const uaLang = document.getElementById('ua-lang');
+uaLang.addEventListener('click', changeLanguageUA)
 
 const ruLang = document.getElementById('ru-lang');
 ruLang.addEventListener('click', changeLanguageRU)
