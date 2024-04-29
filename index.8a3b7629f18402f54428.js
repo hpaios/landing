106a58212df57c0845db17c1f958eae5540a7aa4
Anna <>
2024-04-29 15:54:48 +0300

    Updates

--- index.8a3b7629f18402f54428.js
+++ index.8a3b7629f18402f54428.js
@@ -3,10 +3,20 @@
 var __webpack_exports__ = {};
 
 
-window.addEventListener('load', function (e) {
-  e.preventDefault();
+window.addEventListener('load', function () {
+  // e.preventDefault();
   var lang = localStorage.getItem('lang');
   lang === 'ua' ? changeLanguageUA() : changeLanguageRU();
+
+  // const toggle = document.getElementById('menu-toggle');
+  // const body = document.getElementsByTagName('body');
+  // console.log('aaa')
+  // if(toggle.checked) {  
+  //   console.log('bbbb')
+  //  body[0].style.overflowY = 'hidden';
+  // } else {
+  //   body[0].style.overflowY = 'auto';
+  // }
 });
 var changeLanguageUA = function changeLanguageUA() {
   var ruLang = document.getElementsByClassName("ru-lang");
@@ -26,9 +36,29 @@ var changeLanguageRU = function changeLanguageRU() {
     uaLang[i].style.display = 'none';
   }
 };
+
+// const fixScrollBody = () => {
+//   const body = document.getElementsByTagName('body');
+//   body[0].style.overflowY = 'hidden';
+// }
+
 var uaLang = document.getElementById('ua-lang');
 uaLang.addEventListener('click', changeLanguageUA);
 var ruLang = document.getElementById('ru-lang');
 ruLang.addEventListener('click', changeLanguageRU);
+
+// const body = document.getElementsByTagName('body');
+// const toggle = document.getElementById('menu-toggle');
+// toggle.addEventListener('change', function(e) {
+//   e.preventDefault();
+//   e.stopPropagation();
+//   body[0].style.overflow = 'hidden';
+//   console.log('toggle.checked', toggle.checked)
+//   if (toggle.checked) {
+//     body[0].style.overflow = 'hidden';
+//   } else {
+//     body[0].style.overflow = 'auto';   
+//   }
+// });
 /******/ })()
 ;
