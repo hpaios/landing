341b9d3e6f40a5af187d86818adb7e52ad8d130c
Anna <>
2024-04-29 15:50:42 +0300

    fix menu

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import './styles/styles.scss';
 import './fonts/fonts.scss';
 
-window.addEventListener('load', (e) => {
-  e.preventDefault();
+window.addEventListener('load', () => {
+  // e.preventDefault();
   const lang = localStorage.getItem('lang');
   lang === 'ua' ? changeLanguageUA() : changeLanguageRU();
+
+  // const toggle = document.getElementById('menu-toggle');
+  // const body = document.getElementsByTagName('body');
+  // console.log('aaa')
+  // if(toggle.checked) {  
+  //   console.log('bbbb')
+  //  body[0].style.overflowY = 'hidden';
+  // } else {
+  //   body[0].style.overflowY = 'auto';
+  // }
 })
 
 const changeLanguageUA = () => { 
@@ -27,8 +37,27 @@ const changeLanguageRU = () => {
   }
 };
 
+// const fixScrollBody = () => {
+//   const body = document.getElementsByTagName('body');
+//   body[0].style.overflowY = 'hidden';
+// }
+
 const uaLang = document.getElementById('ua-lang');
-uaLang.addEventListener('click', changeLanguageUA)
+uaLang.addEventListener('click',changeLanguageUA)
 
 const ruLang = document.getElementById('ru-lang');
 ruLang.addEventListener('click', changeLanguageRU)
+
+// const body = document.getElementsByTagName('body');
+// const toggle = document.getElementById('menu-toggle');
+// toggle.addEventListener('change', function(e) {
+//   e.preventDefault();
+//   e.stopPropagation();
+//   body[0].style.overflow = 'hidden';
+//   console.log('toggle.checked', toggle.checked)
+//   if (toggle.checked) {
+//     body[0].style.overflow = 'hidden';
+//   } else {
+//     body[0].style.overflow = 'auto';   
+//   }
+// });
